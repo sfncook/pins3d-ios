a8935160a46a53c667be894e70ffe4d2cacb23ad
Shawn Cook <sfncook@gmail.com>
2023-10-11 08:50:37 -0700

    PickMOduleTypeView

--- Pins3D/Views/PickModuleTypeView.swift
+++ Pins3D/Views/PickModuleTypeView.swift
@@ -4,9 +4,39 @@ struct PickModuleTypeView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
     var body: some View {
-        let newItem = MyItem(context: viewContext)
-//        newItem.timestamp = Date()
-        Text("Third Detail View")
-        // And so on...
+        VStack(spacing: 20) {
+            NavigationLink(destination: ThirdDetailView()) {
+                Text("Facility Scan")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding(.horizontal)
+            
+            NavigationLink(destination: ThirdDetailView()) {
+                Text("Machine Scan")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding(.horizontal)
+            
+            NavigationLink(destination: ThirdDetailView()) {
+                Text("Procedure")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding(.horizontal)
+            
+            .navigationBarTitle("Module type?", displayMode: .inline)
+        }
+        .padding(.vertical)
     }
 }
