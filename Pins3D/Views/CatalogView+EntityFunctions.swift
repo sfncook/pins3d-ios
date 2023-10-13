0a21ba3f20bc7ac316df1208b0e7b4549bab4f4c
Shawn Cook <sfncook@gmail.com>
2023-10-13 10:28:56 -0700

    CatalogView+EntityFunctions

---
+++ Pins3D/Views/CatalogView+EntityFunctions.swift
@@ -0,0 +1,84 @@
+//
+//  CatalogView+EntityFunctions.swift
+//  Pins3D
+//
+//  Created by Shawn Cook on 10/13/23.
+//
+
+import SwiftUI
+import CoreData
+
+extension CatalogView {
+    func deleteMachines(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { machines[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    func deleteProcedures(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { procedures[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    func addItem() {
+        withAnimation {
+            let newItem = MyItem(context: viewContext)
+            newItem.timestamp = Date()
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//#Preview {
+//    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+//}
+
--- Pins3D/Views/CatalogView.swift
+++ Pins3D/Views/CatalogView.swift
@@ -9,27 +9,27 @@ import SwiftUI
 import CoreData
 
 struct CatalogView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.managedObjectContext) var viewContext
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \MyItem.timestamp, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<MyItem>
+    var items: FetchedResults<MyItem>
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Facility.name, ascending: true)],
         animation: .default)
-    private var facilities: FetchedResults<Facility>
+    var facilities: FetchedResults<Facility>
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Machine.name, ascending: true)],
         animation: .default)
-    private var machines: FetchedResults<Machine>
+    var machines: FetchedResults<Machine>
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Procedure.name, ascending: true)],
         animation: .default)
-    private var procedures: FetchedResults<Procedure>
+    var procedures: FetchedResults<Procedure>
     
     enum ModuleType: String {
         case facility
@@ -107,76 +107,4 @@ struct CatalogView: View {
             }// VStack
         }
     }
-
-    private func deleteMachines(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { machines[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteProcedures(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { procedures[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = MyItem(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-//#Preview {
-//    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-//}
